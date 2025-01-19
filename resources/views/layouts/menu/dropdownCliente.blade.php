8c4be5b5bd37d295780e9794d046ef97d627028c
pedrocel <pedrinhonovais10@gmail.com>
2025-01-19 14:40:55 -0300

    feat

--- resources/views/layouts/menu/dropdownCliente.blade.php
+++ resources/views/layouts/menu/dropdownCliente.blade.php
@@ -1,5 +1,5 @@
 <div id="userMenuDropdown" class="hidden absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 shadow-lg rounded-lg z-50">
-    <a href="/" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
+    <a href="/cliente/produtos" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
         Inicio
     </a>
     <a href="/profile" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
