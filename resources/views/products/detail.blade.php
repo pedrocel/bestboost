f1ae957f038482be7b73887515ca467ba13bf321
pedrocel <pedrinhonovais10@gmail.com>
2025-01-20 16:22:53 -0300

    feat product list

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -19,31 +19,49 @@
                 <p class="text-gray-600 text-lg">{{ $product->description }}</p>
 
                 <div class="flex items-center space-x-4">
-                    <!-- Status "Em Alta" -->
+                    <!-- Status "Em Alta" com ícone -->
                     @if($product->is_trending)
-                        <span class="text-green-500 font-bold">Em Alta</span>
+                        <span class="flex items-center text-green-500 font-bold">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#f97316" viewBox="0 0 24 24" stroke="none" class="w-5 h-5 mr-1">
+                                <path d="M12 2C9.65 2 8.17 4.08 8.17 6.56c0 1.08.28 2.1.78 3.01C8.26 9.92 7 8.74 7 7.3 7 5.13 8.78 3 11 3c.37 0 .72.06 1.07.16.54-.95.94-1.7.94-1.7zM15.75 9.73c.4.62.75 1.39.75 2.25 0 3.38-3.21 5.42-3.87 5.85-.68-.43-3.87-2.47-3.87-5.85 0-.69.12-1.38.35-2.03.2.32.46.61.76.85C9.9 9.93 9 9.55 9 9c0-1.57 1.65-2.86 2.58-3.6.53-.42.87-.77 1.13-1.19.15.56.65 1.47 1.5 1.85.45.2.87.42 1.24.69-.26.5-.48 1.06-.7 1.66z" />
+                            </svg>
+                            Em Alta
+                        </span>
                     @endif
 
                     <!-- Preço -->
                     <span class="text-2xl font-bold text-gray-800">R$ {{ number_format($product->price, 2, ',', '.') }}</span>
                 </div>
 
+                <!-- Possibilidade de Lucro -->
+                <p class="text-gray-600">
+                    Possibilidade de Lucro: 
+                    <span class="font-semibold text-green-600">
+                        R$ {{ number_format($product->possible_profit - $product->price, 2, ',', '.') }}
+                    </span>
+                </p>
+
                 <!-- Quantidade de Vendas -->
                 <p class="text-gray-600">Vendas: <span class="font-semibold">{{ $product->sales_count }}</span></p>
 
+                <!-- Ações -->
                 <div class="mt-6 flex space-x-4 items-center">
-                <a href="{{$product->link}}" target="_blank" class="btn-primary bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                    Acessar pagina do produto
-                </a>
-                <form action="{{ route('product.toggleFavorite', $product->id) }}" method="POST">
-                    @csrf
-                    @if($product->isFavoritedByUser(Auth::user()))
-                        <button type="submit" class="btn-primary bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">Remover dos favoritos</button>
-                    @else
-                        <button type="submit" class="btn-primary bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">Adicionar aos favoritos</button>                
-                    @endif
-                </form>
-            </div>
+                    <a href="{{ $product->link }}" target="_blank" class="btn-primary bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                        Acessar página do produto
+                    </a>
+                    <form action="{{ route('product.toggleFavorite', $product->id) }}" method="POST">
+                        @csrf
+                        @if($product->isFavoritedByUser(Auth::user()))
+                            <button type="submit" class="btn-primary bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
+                                Remover dos favoritos
+                            </button>
+                        @else
+                            <button type="submit" class="btn-primary bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+                                Adicionar aos favoritos
+                            </button>
+                        @endif
+                    </form>
+                </div>
             </div>
         </div>
 
@@ -52,7 +70,9 @@
             <h2 class="text-2xl font-semibold text-gray-800">Informações Adicionais</h2>
             <div class="mt-4 p-4 bg-gray-50 rounded-lg shadow-sm">
                 <p class="text-gray-600"><strong>Categoria:</strong> {{ $product->categoryR->name }}</p>
-                <!-- Aqui você pode adicionar outras informações relevantes do produto -->
+                <!-- Adicione mais campos relevantes aqui -->
+                <p class="text-gray-600"><strong>Data de Criação:</strong> {{ $product->created_at->format('d/m/Y') }}</p>
+                <p class="text-gray-600"><strong>Última Atualização:</strong> {{ $product->updated_at->format('d/m/Y') }}</p>
             </div>
         </div>
     </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,14 +4,10 @@
             Biblioteca de Produtos
         </h2>
     </x-slot>
-
     <div class="container mx-auto mt-6 px-4">
-        <!-- Cabeçalho com Filtros e Busca -->
         <div class="mb-6 flex flex-wrap justify-between items-center">
             <h2 class="text-lg font-medium text-white">Produtos</h2>
-
             <form method="GET" action="{{ route('cliente.products.index') }}" class="flex flex-wrap gap-4 items-center w-full sm:w-auto">
-                <!-- Filtros -->
                 <div class="relative w-full sm:w-auto">
                     <select id="category" name="category" class="mt-1 block w-full sm:w-48 p-2 bg-gray-800 text-gray-200 border border-gray-700 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500">
                         <option value="">Todas as Categorias</option>
@@ -22,58 +18,62 @@
                         @endforeach
                     </select>
                 </div>
-
-                <!-- Barra de Busca -->
                 <div class="relative w-full sm:w-auto">
                     <input type="text" id="search" name="search" placeholder="Buscar por nome" value="{{ request('search') }}" class="p-2 w-full sm:w-48 bg-gray-800 text-gray-200 border border-gray-700 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
                 </div>
-
-                <!-- Botão de Filtro -->
                 <button type="submit" class="btn-filter bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
                     Filtrar
                 </button>
             </form>
         </div>
-
-        <!-- Listagem de Produtos -->
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            @foreach($products as $product)
-                <div class="bg-gray-800 rounded-lg shadow-md overflow-hidden hover:scale-105 transform transition duration-300">
-                    <!-- Imagem do Produto -->
-                    <a href="{{ route('cliente.products.detail', $product->id) }}">
-                        <img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="w-full h-48 sm:h-64 object-cover cursor-pointer">
+    @foreach($products as $product)
+        <div class="bg-gray-800 rounded-lg shadow-md overflow-hidden hover:scale-105 transform transition duration-300">
+            <a href="{{ route('cliente.products.detail', $product->id) }}">
+                <img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="w-full h-48 sm:h-64 object-cover cursor-pointer">
+            </a>
+            <div class="p-4">
+                <h3 class="text-lg font-semibold text-white">{{ $product->name }}</h3>
+                <p class="text-gray-400 mt-2">
+                    {{ Str::limit($product->description, 150, '...') }}
+                </p>
+
+                <div class="mt-4 flex justify-between items-center">
+    @if($product->is_trending)
+        <span class="flex items-center text-green-500 font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#f97316" viewBox="0 0 24 24" stroke="none" class="w-5 h-5 mr-1">
+                <path d="M12 2C9.65 2 8.17 4.08 8.17 6.56c0 1.08.28 2.1.78 3.01C8.26 9.92 7 8.74 7 7.3 7 5.13 8.78 3 11 3c.37 0 .72.06 1.07.16.54-.95.94-1.7.94-1.7zM15.75 9.73c.4.62.75 1.39.75 2.25 0 3.38-3.21 5.42-3.87 5.85-.68-.43-3.87-2.47-3.87-5.85 0-.69.12-1.38.35-2.03.2.32.46.61.76.85C9.9 9.93 9 9.55 9 9c0-1.57 1.65-2.86 2.58-3.6.53-.42.87-.77 1.13-1.19.15.56.65 1.47 1.5 1.85.45.2.87.42 1.24.69-.26.5-.48 1.06-.7 1.66z" />
+            </svg>
+            Em Alta
+        </span>
+    @endif
+    <div class="text-right ml-auto">
+        <!-- Mostrar o preço -->
+        <span class="text-lg font-semibold text-gray-300">R$ {{ number_format($product->price, 2, ',', '.') }}</span>
+        <br>
+        <!-- Mostrar a possibilidade de lucro -->
+        <span class="text-sm text-gray-400">
+            Lucro: 
+            R$ {{ number_format($product->possible_profit - $product->price, 2, ',', '.') }}
+        </span>
+    </div>
+</div>
+
+
+                <!-- Informações Adicionais -->
+                <div class="mt-2 flex justify-between items-center">
+                    <span class="text-sm text-gray-400">Vendas: {{ $product->sales_count }}</span>
+
+                    <!-- Botão Ver Detalhes -->
+                    <a href="{{ route('cliente.products.detail', $product->id) }}" class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-6 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                        Ver Detalhes
                     </a>
-
-                    <div class="p-4">
-                        <!-- Nome do Produto -->
-                        <h3 class="text-lg font-semibold text-white">{{ $product->name }}</h3>
-
-                        <!-- Descrição do Produto -->
-                        <p class="text-gray-400 mt-2">
-                            {{ Str::limit($product->description, 150, '...') }}
-                        </p>
-
-                        <!-- Preço e Status -->
-                        <div class="mt-4 flex justify-between items-center">
-                            @if($product->is_trending)
-                                <span class="text-green-500 font-bold">Em Alta</span>
-                            @endif
-                            <span class="text-lg font-semibold text-gray-300">R$ {{ number_format($product->price, 2, ',', '.') }}</span>
-                        </div>
-
-                        <!-- Informações Adicionais -->
-                        <div class="mt-2 flex justify-between items-center">
-                            <span class="text-sm text-gray-400">Vendas: {{ $product->sales_count }}</span>
-
-                            <!-- Botão Ver Detalhes -->
-                            <a href="{{ route('cliente.products.detail', $product->id) }}" class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-6 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                                Ver Detalhes
-                            </a>
-                        </div>
-                    </div>
                 </div>
-            @endforeach
+            </div>
         </div>
+    @endforeach
+</div>
+
 
         <!-- Paginação -->
         <div class="mt-6 text-gray-300">
